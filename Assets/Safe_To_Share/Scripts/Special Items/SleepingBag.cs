7e63e623fdeacd2d68dc390b9ba1cec18131ec70
Ugre101 <westin.gustav@gmail.com>
2022-09-18 00:58:12 +0200

    sleeping bag

---
+++ Assets/Safe_To_Share/Scripts/Special Items/SleepingBag.cs
@@ -0,0 +1,17 @@
+﻿using Character;
+using Items;
+using UnityEngine;
+
+namespace Safe_To_Share.Scripts.Special_Items
+{
+    [CreateAssetMenu(menuName = "Items/Special Items/Create SleepingBag", fileName = "SleepingBag", order = 0)]
+    public class SleepingBag : Item
+    {
+        [SerializeField] int sleepQuality = 50;
+        public override void Use(BaseCharacter user)
+        {
+            base.Use(user);
+            user.BaseSleep(sleepQuality);
+        }
+    }
+}
