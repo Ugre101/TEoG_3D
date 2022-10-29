5e39e062a459ff64c0370e6a38e925931669ca2a
Ugre101 <westin.gustav@gmail.com>
2022-10-30 00:11:30 +0200

    piss prefab start

---
+++ Assets/Safe_To_Share/Scripts/AvatarStuff/ScatAndPiss/PissPrefab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace AvatarStuff
+{
+    public class PissPrefab : MonoBehaviour
+    {
+        [SerializeField] Rigidbody rigid;
+        public void Launch(Vector3 dir)
+        {
+            rigid.AddRelativeForce(dir);
+        }
+
+        void OnCollisionStay(Collision collisionInfo)
+        {
+            
+        }
+    }
+}
--- Assets/Safe_To_Share/Scripts/AvatarStuff/ScatAndPiss/ScatHandler.cs
+++ Assets/Safe_To_Share/Scripts/AvatarStuff/ScatAndPiss/ScatHandler.cs
@@ -5,11 +5,16 @@ namespace AvatarStuff
     public class ScatHandler : MonoBehaviour
     {
         [SerializeField] ScatPrefab prefab;
-
+        [SerializeField] GameObject pissPrefab;
         public void Scat(int size)
         {
             ScatPrefab scat = Instantiate(prefab, transform);
             scat.Play();
+        }
+
+        public void Piss(float pressure)
+        {
+            
         }
     }
 }
